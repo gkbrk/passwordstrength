bdc82aef2a21a68fd07593f4ef12dd0e08dbc810
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-08-27 15:48:13 +0200

    Added more info and included the dictionary file in the install

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import *
 
 kwargs = {
-	"description" : "A password strength checker.",
-	"entry_points" : {"console_scripts" : ["passwordstrength=passwordstrength.passwordstrength:main"]},
-	"name" : "passwordstrength",
-	"packages" : ["passwordstrength"],
+	'name' : 'passwordstrength',
+	'version' : '0.1',
+	'description' : 'A password strength checker.',
+	'entry_points' : {'console_scripts' : ['passwordstrength=passwordstrength.passwordstrength:main']},
+	'packages' : ['passwordstrength'],
+	'package_data' : {'passwordstrength': ['english']},
+	'include_package_data' : True,
 }
 
 setup(**kwargs)
